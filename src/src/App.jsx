0e089b13e1635421f287fbe48fd11bf29b4e0173
develop_yunju <ilove4903@naver.com>
2024-05-05 18:52:15 +0900

    FlowNary-final

--- src/src/App.jsx
+++ src/src/App.jsx
@@ -7,7 +7,7 @@ const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={queryClient}> 
       <LocalizationProvider dateAdapter={AdapterDateFns}>                   
         <Outlet />          
       </LocalizationProvider>
